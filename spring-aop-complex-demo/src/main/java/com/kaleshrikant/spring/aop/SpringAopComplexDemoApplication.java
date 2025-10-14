9692a2bb0554eef4d28db69cb7c09ba9e7e1e0d2
Shrikant Kale <kaleshrikant@live.com>
2025-10-14 11:39:37 +0530

    initial commit

---
+++ spring-aop-complex-demo/src/main/java/com/kaleshrikant/spring/aop/SpringAopComplexDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopComplexDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopComplexDemoApplication.class, args);
+	}
+
+}
