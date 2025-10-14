36bd9885d57f3c0e3c99927118841f5d70c57d12
Shrikant Kale <kaleshrikant@live.com>
2025-10-14 07:51:16 +0530

    Initial Commit

---
+++ spring-boot-aop-demo/src/main/java/com/kaleshrikant/spring/aop/SpringBootAopDemoApplication.java
@@ -0,0 +1,14 @@
+package com.kaleshrikant.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopDemoApplication.class, args);
+
+	}
+
+}
