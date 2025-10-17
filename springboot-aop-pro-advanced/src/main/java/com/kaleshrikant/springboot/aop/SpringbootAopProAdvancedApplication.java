805c7a88964cb979e63301bf0e768a6fd909330b
Shrikant Kale <kaleshrikant@live.com>
2025-10-17 09:32:21 +0530

    Initial Commit

---
+++ springboot-aop-pro-advanced/src/main/java/com/kaleshrikant/springboot/aop/SpringbootAopProAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.kaleshrikant.springboot.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopProAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAopProAdvancedApplication.class, args);
+	}
+
+}
